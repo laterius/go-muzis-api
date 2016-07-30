bf4e778ebaf60bff0dec4028b30b1d3c8c8c8eb6
Denis <denis.gluchov@gmail.com>
2016-07-30 15:00:47 +0300

    test commit

---
+++ api.go
@@ -0,0 +1,9 @@
+package go_muzis_api
+
+func InvokeFromExternalPackage() string {
+	return "InvokeFromExternalPackage"
+}
+
+func myFunction() string{
+	return "sdf"
+}
---
+++ api_test.go
@@ -0,0 +1,1 @@
+package go_muzis_api
